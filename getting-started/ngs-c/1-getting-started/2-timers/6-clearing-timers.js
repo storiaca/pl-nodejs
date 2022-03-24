2d962f5eddb00a62eeed9ed6bcc893a642bef289
Aleksandar Ristic <storiaca@gmail.com>
2022-03-24 08:54:14 +0100

    Process object start

--- getting-started/ngs-c/1-getting-started/2-timers/6-clearing-timers.js
+++ getting-started/ngs-c/1-getting-started/2-timers/6-clearing-timers.js
@@ -1,6 +1,8 @@
-const timerId = setTimeout(
-  () => console.log('You will not see this one!'),
-  0
-);
+const timerId = setTimeout(() => console.log("You will not see this one!"), 0);
+
+// setImmediate
 
 clearTimeout(timerId);
+
+// clearInterval
+// clearImmediate
--- getting-started/ngs-c/1-getting-started/2-timers/8-challenge2.js
+++ getting-started/ngs-c/1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,13 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+let counter = 0;
+const intervalId = setInterval(() => {
+  console.log("Hello World");
+  counter += 1;
+
+  if (counter === 5) {
+    console.log("Done");
+    clearInterval(intervalId);
+  }
+}, 1000);
