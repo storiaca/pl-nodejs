1fd8db393dbb0a93ff0aaaff81a3b271e3286c8d
Aleksandar Ristic <storiaca@gmail.com>
2022-03-30 08:42:20 +0200

    Object literals

--- getting-started/ngs-c/2-modern-js/7-objects.js
+++ getting-started/ngs-c/2-modern-js/7-objects.js
@@ -1,6 +1,13 @@
+const mystery = "answer";
+
 const obj = {
   p1: 10,
   p2: 20,
   f1() {},
   f2: () => {},
+  [mystery]: 42,
 };
+
+console.log(obj.mystery); // undefined
+
+console.log(obj.answer); // 42
