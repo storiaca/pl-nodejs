9b8f4f1e1b50cafa18c6ddbb165e3e1ad8d75b14
Aleksandar Ristic <storiaca@gmail.com>
2022-04-08 08:51:39 +0200

    Hello world node version

--- getting-started/ngs-c/5-web/1-hello-world.js
+++ getting-started/ngs-c/5-web/1-hello-world.js
@@ -1,9 +1,14 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
-});
+const requestListener = (req, res) => {
+  res.write("Hello World\n");
+  res.end();
+};
+
+const server = http.createServer();
+
+server.on("request", requestListener);
 
 server.listen(4242, () => {
-  console.log('Server is running...');
+  console.log("Server is running...");
 });
