d8fc1d8b4f039e3d3608d4fcf7fb46898ba56382
Aleksandar Ristic <storiaca@gmail.com>
2022-04-15 08:38:28 +0200

    Repl and cli

--- advanced-nodejs/1.4/repl.js
+++ advanced-nodejs/1.4/repl.js
@@ -1,8 +1,12 @@
-const repl = require('repl');
+const repl = require("repl");
 
 let r = repl.start({
   ignoreUndefined: true,
-  replMode: repl.REPL_MODE_STRICT
+  replMode: repl.REPL_MODE_STRICT,
 });
 
-r.context.lodash = require('lodash');
+r.context.lodash = require("lodash");
+
+// https://nodejs.org/en/knowledge/REPL/how-to-use-nodejs-repl/#:~:text=Starting%20the%20REPL%20is%20simple,command%20line%20without%20a%20filename.&text=It%20then%20drops%20you%20into,history%20and%20modify%20previous%20commands.
+
+// https://nodejs.org/api/repl.html#replstartoptions
