634b4230beb2d718537c04cdaf41afecf24d20bc
Aleksandar Ristic <storiaca@gmail.com>
2022-04-27 08:41:11 +0200

    Buffer documentatuon links

--- advanced-nodejs/1.5/buffer.js
+++ advanced-nodejs/1.5/buffer.js
@@ -1,5 +1,8 @@
-const string = 'touché';
-const buffer = Buffer.from('touché');
+const string = "touché";
+const buffer = Buffer.from("touché");
 
 console.log(string, string.length);
 console.log(buffer, buffer.length);
+
+// https://nodejs.org/api/buffer.html
+// https://nodejs.org/api/buffer.html#buffer
